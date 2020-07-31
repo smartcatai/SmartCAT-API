2aee4732b42e9ed375966d076272eec2ab118105
Andrey Savenkov <a.savenkov@smartcat.ai>
2020-07-31 13:06:21 +0300

    added getInvoiceList method

--- src/SmartCAT/API/Resource/InvoiceResource.php
+++ src/SmartCAT/API/Resource/InvoiceResource.php
@@ -4,6 +4,9 @@ namespace SmartCat\Client\Resource;
 
 use SmartCat\Client\Helper\QueryParam;
 
+/**
+ * @property string $host
+ */
 class InvoiceResource extends Resource
 {
     /**
@@ -32,13 +35,12 @@ class InvoiceResource extends Resource
     }
 
     /**
-     *
-     *
      * @param \SmartCat\Client\Model\ImportJobModelV2 $model
-     * @param array  $parameters List of parameters
-     * @param string $fetch      Fetch mode (object or response)
+     * @param array $parameters List of parameters
+     * @param string $fetch Fetch mode (object or response)
      *
-     * @return \Psr\Http\Message\ResponseInterface
+     * @return \Psr\Http\Message\ResponseInterface|string|string[]
+     * @throws \Exception
      */
     public function invoiceImportJobV2(\SmartCat\Client\Model\ImportJobModelV2 $model, $parameters = array(), $fetch = self::FETCH_OBJECT)
     {
@@ -63,6 +65,14 @@ class InvoiceResource extends Resource
         return $response;
     }
 
+    /**
+     * @param \SmartCat\Client\Model\CreateInvoiceModel $model
+     * @param array $parameters
+     * @param string $fetch Fetch mode (object or response)
+     *
+     * @return \Psr\Http\Message\ResponseInterface|string|string[]
+     * @throws \Exception
+     */
     public function createInvoice(\SmartCat\Client\Model\CreateInvoiceModel $model, $parameters = array(), $fetch = self::FETCH_OBJECT)
     {
         $queryParam = new QueryParam();
@@ -85,6 +95,13 @@ class InvoiceResource extends Resource
         return $response;
     }
 
+    /**
+     * @param $externalIds
+     * @param string $fetch Fetch mode (object or response)
+     *
+     * @return \Psr\Http\Message\ResponseInterface|string|string[]
+     * @throws \Exception
+     */
     public function jobListByExternalId($externalIds, $fetch = self::FETCH_OBJECT)
     {
         $queryParam = new QueryParam();
@@ -98,6 +115,41 @@ class InvoiceResource extends Resource
         } else {
             $url = $url . '?externalIds=' . $externalIds;
         }
+        $headers = array_merge(array('Accept' => array('application/json'), 'Content-Type' => 'application/json'));
+        $request = $this->messageFactory->createRequest('GET', $url, $headers);
+        $promise = $this->httpClient->sendAsyncRequest($request);
+        if (self::FETCH_PROMISE === $fetch) {
+            return $promise;
+        }
+        $response = $promise->wait();
+        if (self::FETCH_OBJECT == $fetch) {
+            if ('200' == $response->getStatusCode()) {
+                $body = (string) $response->getBody();
+                return json_decode($body);
+            }
+        }
+        return $response;
+    }
+
+    /**
+     * @param $dateCreatedFrom
+     * @param $dateCreatedTo
+     * @param int $limit
+     * @param int $skip
+     * @param string $fetch Fetch mode (object or response)
+     *
+     * @return \Psr\Http\Message\ResponseInterface|string|string[]
+     * @throws \Exception
+     */
+    public function getInvoiceList(\DateTime $dateCreatedFrom, \DateTime $dateCreatedTo, $limit = 10, $skip = 0, $fetch = self::FETCH_OBJECT)
+    {
+        $queryParam = new QueryParam();
+        $url = $this->host . '/api/integration/v2/invoice/list';
+        $url = $url . '?dateCreatedFrom=' . $dateCreatedFrom->format('Y-m-d H:i:s');
+        $url = $url . '&dateCreatedTo=' . $dateCreatedTo->format('Y-m-d H:i:s');
+        $url = $url . '&limit=' . $limit;
+        $url = $url . '&skip=' . $skip;
+
         $headers = array_merge(array('Accept' => array('application/json'), 'Content-Type' => 'application/json'));
         $request = $this->messageFactory->createRequest('GET', $url, $headers);
         $promise = $this->httpClient->sendAsyncRequest($request);
